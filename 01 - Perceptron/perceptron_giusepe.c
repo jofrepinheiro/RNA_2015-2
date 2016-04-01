0c6595b660743806de7e1075f57d2e89c49058df
Giusepe23 <giusepe@outlook.com>
2016-04-01 01:09:35 -0300

    Código pronto, mas os pesos sempre vão para 0

--- 01 - Perceptron/perceptron_giusepe.c
+++ 01 - Perceptron/perceptron_giusepe.c
@@ -2,15 +2,13 @@
 
 int main (int argc, char** argv)
 {
-	/* Rede */
 	int ann[3][4];
-	/* Resultado desejado */
+	int pesos[4];
 	int saida[4];
-	float taxaAprendizado = 0.1;
-	int epoca = 0;
-	int erro = 1;
-	int i = 0; int j = 0; int y = 0;
-	int bias = 1;
+
+	float taxaAprendizado = 0.3; float ativacao = 0.3;
+
+	int epoca = 0; int erro = 1; int i = 0; int j = 0; int y = 0; int bias = 0;
 
     ann[0][0] = 1;
     ann[0][1] = 1;
@@ -27,26 +25,55 @@ int main (int argc, char** argv)
     ann[2][2] = 1;
     ann[2][3] = 1;
 
+    pesos[0] = 1;
+    pesos[1] = 1;
+    pesos[2] = 1;
+    pesos[3] = 1;
+
     saida[0] = 1;
     saida[1] = -1;
     saida[2] = -1;
     saida[3] = -1;
 
-    while(erro == 1)
+    while(erro != 4)
     {
     	for (i = 0; i < 3; i++)
     	{
     		for (j = 0; j < 4; j++)
+    			y += bias + ann[i][j] * pesos[j];
+
+    		printf("y = %d\n", y);
+
+    		if (y > ativacao)
+    			y = 1;
+    		else
+    			if (y < (ativacao*(-1)))
+    				y = -1;
+    			else
+    				y = 0;
+
+    		if (y != saida[i])
     		{
-    			y += bias + ann[i][j] * saida[j];
+    			for (j = 0; j < 4; j++)
+    				pesos[j] = pesos[j] + taxaAprendizado * saida[i] * ann[i][j];
+
+    			bias = bias + taxaAprendizado * saida[i];
+    			erro = 0;
+    		}
+    		else
+    		{
+    			erro++;
     		}
 
-
-
-    		erro = 0;
     		y = 0;
     	}
     	epoca++;
+    	for (j = 0; j < 4; j++)
+    	{
+    		printf("Peso %d: %d\n", j, pesos[j]);
+    	}
+
+    	system("pause");
     }
 
     return (0);
