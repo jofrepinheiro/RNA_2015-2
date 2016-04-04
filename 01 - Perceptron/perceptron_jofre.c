c87bd8af0a34bb0963badf7618ba538814b023d8
jofrepinheiro <jofrepinheiro@gmail.com>
2016-04-04 19:53:28 -0300

    Mudança de tipo

--- 01 - Perceptron/perceptron_jofre.c
+++ 01 - Perceptron/perceptron_jofre.c
@@ -3,46 +3,43 @@
 int main()
 {
   
-	  int x1[4];
-	  int x2[4];
-	  int x3[4];
-	  int controle[4];
-	  int w[4];
-	  int t[4];
+	  float x[4][4];
+	  float controle[4];
+	  float w[4];
+	  float t[4];
 	  int b = 0, i=0, epoca = 0, y = 0, y_in = 0, parada = 0;
 	  float theta = 0.2, alfa = 0.1;
   
   
-  x1[0] = 1;
-  x1[1] = 1;
-  x1[2] = 1;
-  x1[3] = 0;
+  x[0][0] = 1;
+  x[0][1] = 1;
+  x[0][2] = 1;
+  x[0][3] = 0;
   
-  x2[0] = 1;
-  x2[1] = 1;
-  x2[2] = 0;
-  x2[3] = 1;
+  x[1][0] = 1;
+  x[1][1] = 1;
+  x[1][2] = 0;
+  x[1][3] = 1;
   
-  x3[0] = 1;
-  x3[1] = 0;
-  x3[2] = 1;
-  x3[3] = 1;
+  x[2][0] = 1;
+  x[2][1] = 0;
+  x[2][2] = 1;
+  x[2][3] = 1;
   
+  x[3][0] = 1;
+  x[3][1] = 1;
+  x[3][2] = 1;
+  x[3][3] = 1;
+ 
   t[0] =  1;
   t[1] = -1;
   t[2] = -1;
   t[3] = -1;
   
-  w[0] = -1;
-  w[1] = -1;
-  w[2] = -1;
-  w[3] = -1;
-  
-  controle[0] = 1;
-  controle[1] = 1;
-  controle[2] = 1;
-  controle[3] = 1;
-  
+  w[0] = 0;
+  w[1] = 0;
+  w[2] = 0;
+  w[3] = 0;
   
   while(parada == 0){
 	for(i=0; i<4; i++){
@@ -67,6 +64,7 @@ int main()
 		}	
 		
 		printf("%d %d %d %d %d\n", epoca, w[0], w[1], w[2], w[3]);
+		system("pause");
 	}
 	epoca = epoca + 1;	
   }
