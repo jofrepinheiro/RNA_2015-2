67987f205fd76011054b35137e807acd624b4cf4
Giusepe23 <giusepe@outlook.com>
2016-08-03 10:23:53 -0300

    Edições

--- 02 - Perceptron - Reconhecimento de Padrões/p02_giusepe.c
+++ 02 - Perceptron - Reconhecimento de Padrões/p02_giusepe.c
@@ -1,23 +1,29 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 int main ()
 {
 	char ann[7][63];
-	float pesos[63];
+	float pesos[7][63];
 	float saida[7][7];
 
-	float taxaAprendizado = 1; float ativacao = 0.1;
+	float taxaAprendizado = 1.0;
+	float ativacao = 0.0;
 
 	int epoca = 1; int acerto = 1;
-	float y = 0; float bias = 0;
+	float y = 0;
+	float bias[7];
 	int i = 0; int j = 0; int k = 0; int count = 0; int letras = 0;
+
+	for (i=0; i<7; i++)
+		bias[i]=0.0;
 
     char url[]="letras.txt";
 	char ch;
 	FILE *arq;
 
-    for (i = 0; i < 63; i++)
-        pesos[i] = 0;
+    for (i = 0; i < 7; i++)
+    	for (k = 0; k < 63; k++)
+    		pesos[i][k] = 0.0;
 
 	arq = fopen(url, "r");
 	if(arq == NULL)
@@ -26,7 +32,6 @@ int main ()
     {
 	    while( ( (ch=fgetc(arq))!= EOF ) && (letras < 7) )
         {
-
             if (ch != '\n')
             {
                 ann[letras][count] = ch;
@@ -50,44 +55,50 @@ int main ()
     }
 	fclose(arq);
 
-
     /* CÓDIGO DA REDE */
-    while(acerto != 6)
-    {
-        printf("------------- Epoca %d -------------\n", epoca);
 
-        for (i = 0; i < 7; i++)
-        {
-            for (j = 0; j < 63; j++)
-                y += bias + ann[i][j] * pesos[j];
-            if (y > ativacao)
-                y = 1;
-            else if (y < (ativacao*(-1)))
-                y = -1;
-            else
-                y = 0;
-            printf("y%d: %6.2f\n", i, y);
-            if (y != saida[i][i])
-            {
-                for (j = 0; j < 63; j++)
-                    pesos[j] = pesos[j] + taxaAprendizado * saida[i][i] * ann[i][j];
+	for(k = 0; k < 7; k++){
+		acerto = 0;
+		while(acerto == 0)
+		{
+			printf("------------- Epoca %d -------------\n", epoca);
 
-                bias = bias + taxaAprendizado * saida[i][i];
-                acerto = 0;
-            }
-            else
-            {
-                acerto++;
-            }
+			for (i = 0; i < 7; i++)
+			{
+				for (j = 0; j < 63; j++)
+					y += ann[i][j] * pesos[k][j];
+				y += bias[k];
+				if (y > ativacao)
+					y = 1;
+				else if (y < (ativacao*(-1)))
+					y = -1;
+				else
+					y = 0;
+				printf("y%d: %6.2f\n", i, y);
+				if (y != saida[k][i])
+				{
+					for (j = 0; j < 63; j++)
+						pesos[k][j] = pesos[k][j] + taxaAprendizado * saida[k][i] * ann[i][j];
 
-            y = 0;
-        }
+					bias[k] = bias[k] + taxaAprendizado * saida[k][i];
+					acerto = 0;
+				}
+				else
+				{
+					acerto = 1;
+				}
 
-        for (j = 0; j < 63; j++)
-            printf("Peso %d: %6.2f\n", j, pesos[j]);
-        epoca++;
-        printf("\n Acerto: %d", acerto);
-        printf("\n");
+				y = 0;
+			}
+
+			for (j = 0; j < 63; j++)
+				printf("Peso [%d, %d]: %6.2f\n", k, j, pesos[k][j]);
+			epoca++;
+			printf("\n Acerto: %d", acerto);
+			printf("\n");
+//			system("pause");
+		}
+
         /* FIM CÓDIGO DA REDE */
     }
 
